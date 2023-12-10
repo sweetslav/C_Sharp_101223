36d63769f50e2be50cfa344fe01556752cf8eba1
Svyatoslav <t1ksy42@gmail.com>
2023-12-10 21:04:57 +0300

    Homework 2_4

---
+++ lesson_2/Homework 2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает случайное число, а на выходе
+// показывает его цифры через запятую
+
+Random rnd = new Random();
+int randomNumber = rnd.Next(1, 100000); // Генерируем случайное число от 1 до 99999
+Console.WriteLine(randomNumber);
+
+string finalString = "";
+
+while (randomNumber != 0)
+    {
+    int lastDigit = randomNumber % 10;
+    finalString = lastDigit + ", " + finalString;
+    randomNumber /= 10;
+    }
+
+Console.WriteLine(finalString);
