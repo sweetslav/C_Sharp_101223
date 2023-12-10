b73c64a703d4b0e73fa0e8733ce9f298b6b443ae
Svyatoslav <t1ksy42@gmail.com>
2023-12-11 01:27:49 +0300

    Homework 3.3

--- lesson_3/Homework 3_3/Program.cs
+++ lesson_3/Homework 3_3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] floatingArray = { 1.5, 2.8, 3.1, 5.6, 4.2, 7.9 };
+double min = floatingArray[0];
+double max = floatingArray[0];
+
+for (int i = 1; i < floatingArray.Length; i++)
+{
+    if (floatingArray[i] < min)
+    {
+        min = floatingArray[i];
+    }
+    if (floatingArray[i] > max)
+    {
+        max = floatingArray[i];
+    }
+}
+
+double difference = max - min;
+Console.WriteLine($"Разница между максимальным и минимальным элементами: {difference}");
