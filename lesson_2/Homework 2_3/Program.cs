d9f8ce6aabc51ac49f888013aba8406641cf6786
Svyatoslav <t1ksy42@gmail.com>
2023-12-10 20:24:34 +0300

    Homework 2_3

---
+++ lesson_2/Homework 2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход случайное целое число из отрезка 10 - 99
+// и показывает наибольшую цифру этого числа
+
+Random rnd = new Random();
+int randomNumber = rnd.Next(10, 100); // Генерируем случайное число от 10 до 99
+
+Console.WriteLine("Случайное число из отрезка 10-99: " + randomNumber);
+
+int firstDigit = randomNumber / 10;
+int secondDigit = randomNumber % 10;
+
+if (firstDigit > secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    } 
+else if (secondDigit > firstDigit)
+    {
+        Console.WriteLine(secondDigit);
+    }
