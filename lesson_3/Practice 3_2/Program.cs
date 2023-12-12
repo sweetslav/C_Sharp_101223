c82b67ef908da385520fe27bbf592531750d9cae
Svyatoslav <t1ksy42@gmail.com>
2023-12-13 02:40:22 +0300

    Task 3_2

---
+++ lesson_3/Practice 3_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Замените отирцательные элементы на положительные и наоборот
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] MassNums(int size, int from, int to)
+        {
+            int[] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = new Random().Next(from, to + 1);
+            }
+            return arr;
+        }
+        void Print(int[] arr)
+        {
+            int size = arr.Length;
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        void ChangeValue(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = -arr[i];
+            }
+        }
+
+        int num = int.Parse(Console.ReadLine()!);
+        int start = int.Parse(Console.ReadLine()!);
+        int stop = int.Parse(Console.ReadLine()!);
+
+        int[] mass = MassNums(num, start, stop);
+        Print(mass);
+        ChangeValue(mass);
+        Print(mass);
+
+    }
+}
