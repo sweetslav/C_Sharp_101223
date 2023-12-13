25952edfda97609450c98389ff2b74ecb47a053f
Svyatoslav <t1ksy42@gmail.com>
2023-12-13 03:08:49 +0300

    Task 3_4

---
+++ lesson_3/Practice 3_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// создать массив из числа и развернуть его
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] MassNums(int num)
+        {
+            int[] arr = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                arr[i] = num % 10;
+                num /=10;
+            }
+            return arr;
+        }
+        void Print(int[] arr)
+        {
+            int size = arr.Length;
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        int number = new Random().Next(100, 1000);
+        Console.WriteLine(number);
+        int[] mass = MassNums(number);
+        Print(mass);        
+        
+    }
+}
+
