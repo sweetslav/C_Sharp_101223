302b03ba7fa3241c3d23396842b099876f1520fb
Svyatoslav <t1ksy42@gmail.com>
2023-12-14 23:31:30 +0300

    Сделал домашнее задание к Семинару №4

---
+++ Lesson 4/Homework 4.2/Program.cs
@@ -0,0 +1,67 @@
+﻿//  Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void Print(int[] arr) // Вывод массива
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size) // Создание массива ТРЕХЗНАЧНЫХ чисел
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 999);
+    }
+    return arr;
+}
+
+void SelectionSort(int[] array) // Сортировка массива
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition])
+                minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+int EvenNum(int[] arr) // Подсчёт четных
+{
+    int count = 0;
+
+    foreach (int number in arr)
+    {
+        if (number % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num);
+SelectionSort(mass);
+Print(mass);
+
+int result = EvenNum(mass);
+Console.WriteLine();
+
+Console.WriteLine($"Количество четных чисел: {result}");
+Console.WriteLine();
+
+
---
+++ Lesson 4/Homework 4.3/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to + 1);
+    }
+    return arr;
+}
+
+int[] ReverseArray(int[] arr)
+{
+    int start = 0;
+    int end = arr.Length - 1;
+
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+    return arr;
+}
+
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите начало массива");
+int start = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите конец массива");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = MassNums(num, start, stop);
+
+Console.WriteLine();
+Console.WriteLine($"Ваш массив размером: {num} чисел. От: {start} до: {stop}");
+Console.WriteLine();
+Print(mass);
+
+int[] reverseMass = ReverseArray(mass);
+Console.WriteLine();
+Console.WriteLine($"Ваш массив наоборот: ");
+Console.WriteLine();
+Print(reverseMass);
+
