cd361f50a09dbe381137ea27f57530732d31a7cc
Svyatoslav <t1ksy42@gmail.com>
2023-12-20 12:39:57 +0300

    Добавил ветку Lesson_6 + добавил эталонное решение в задачу 5_1

---
+++ Lesson_6/Practice 6_1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i,j] = new Random().Next(from, to + 1);
+        }   
+    }
+    return arr;
+}
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
