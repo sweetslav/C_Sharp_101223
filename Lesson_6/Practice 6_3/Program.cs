67c7562756662be34fe1537ad7b91c7d32caccff
Svyatoslav <t1ksy42@gmail.com>
2023-12-26 15:29:06 +0300

    Clear errors

---
+++ Lesson_6/Practice 6_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре. Выяснить, сколько среди введённых букв гласных.
+
+int CountVowels(string word) // Метод считает количество гласных букв в тексте
+{
+    char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+    int count = 0;
+
+    foreach (var item in word)
+    {
+        if (Array.IndexOf(vowels, item) != -1) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+Console.Write("Input text: ");
+string text = Console.ReadLine()!;
+
+int res = CountVowels(text);
+Console.WriteLine(res);
