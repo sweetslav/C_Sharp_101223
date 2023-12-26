392193604b8c1474db95db26e4e7cac05f76fbaa
Svyatoslav <t1ksy42@gmail.com>
2023-12-26 15:28:20 +0300

    Сделал задания к Семинару №6

--- Lesson_6/Practice 6_1/Program.cs
+++ Lesson_6/Practice 6_1/Program.cs
@@ -1,32 +1,15 @@
-﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+﻿//  Задайте массив символов (тип char []). Создайте строку из символов этого массива. Указание Конструктор строки вида string(char []) не использовать.
 
-int[,] MassNums(int row, int column, int from, int to)
+string ChArrToStr(char[] arr)
 {
-    int[,] arr = new int[row, column];
-
-    for (int i = 0; i < row; i++)
+    string result = "";
+    foreach (var item in arr)
     {
-        for (int j = 0; j < column; j++)
-        {
-            arr[i,j] = new Random().Next(from, to + 1);
-        }   
+        result += item;
     }
-    return arr;
+    return result;
 }
 
-
-void Print(int[,] arr)
-{
-    int row_size = arr.GetLength(0);
-    int column_size = arr.GetLength(1);
-
-    for (int i = 0; i < row_size; i++)
-    {
-        for (int j = 0; j < column_size; j++)
-        {
-            Console.Write($" {arr[i, j]} ");
-        Console.WriteLine();
-        }
-    }
-    Console.WriteLine();
-}
+char[] massChar = {'P', 'e', 'o', 'p', 'l', 'e'};
+string res = ChArrToStr(massChar);
+Console.WriteLine(res);
---
+++ Lesson_6/Practice 6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// На основе символов строки (тип string) сформировать массив символов (тип char[]). Вывести массив на экран. Указание Метод строки ToCharArray() не использовать.
+
+char[] StrToChArr(string word) // Метод преобразует строку в символы
+{
+    int len = word.Length;
+    char[] result = new char[len];
+
+    for (int i = 0; i < len; i++)
+        result[i] = word[i];
+    
+    return result;
+}
+
+string hi = "Hello";
+char[] charArr = StrToChArr(hi);
+
+foreach (var item in charArr)
+{
+    Console.Write(item + ", ");
+}
